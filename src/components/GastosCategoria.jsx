c61c01f07883df528be2e5ac171e1481863dbfe5
kelmita22 <kelmita22@gmail.com>
2021-07-24 17:24:40 -0500

    ruteo componentes

---
+++ src/components/GastosCategoria.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const GastosCategoria = () => {
+    return (
+        <h1>GastosCategoria</h1>
+    );
+}
+
+export default GastosCategoria;
---
+++ src/components/InicioSesion.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const InicioSesion = () => {
+    return (
+        <h1>InicioSesion</h1>
+    );
+}
+
+export default InicioSesion;
