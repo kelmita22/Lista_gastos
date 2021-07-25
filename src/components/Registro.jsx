d43cd45a46043953e2725937c2dd515e6f2e0cbb
kelmita22 <kelmita22@gmail.com>
2021-07-25 16:52:53 -0500

    encabezado registro

--- src/components/Registro.jsx
+++ src/components/Registro.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
+import { ContenedorHeader, Header, Titulo } from '../contentcomponents/Header';
+import Boton from './../contentcomponents/Boton';
 
 const Registro = () => {
     return (
-        <h1>Registro</h1>
+        <>
+            <Helmet>
+                <title>Crear Cuenta</title>
+            </Helmet>
+            <Header>
+                <ContenedorHeader>
+                    <Titulo>Crear Cuenta</Titulo>
+
+                    <section>
+                        <Boton to='/inicio-sesion'>Iniciar Sesión</Boton>
+                    </section>
+                </ContenedorHeader>
+            </Header>
+        </>
     );
 }
 
