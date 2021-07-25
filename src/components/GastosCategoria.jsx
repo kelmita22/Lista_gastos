fbfcc7ba63291d00d6f14ae974fdd6a5c57bdcb7
kelmita22 <kelmita22@gmail.com>
2021-07-25 16:53:15 -0500

    encabezado gastos

--- src/components/GastosCategoria.jsx
+++ src/components/GastosCategoria.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import Helmet from 'react-helmet';
+import { Header, Titulo } from '../contentcomponents/Header';
+import Regresar from './../contentcomponents/Regresar'
 
 const GastosCategoria = () => {
     return (
-        <h1>GastosCategoria</h1>
+        <>
+            <Helmet>
+                <title>Gastos por Categoría</title>
+            </Helmet>
+            <Header>
+                <Regresar />
+                <Titulo>Gastos por Categoría</Titulo>
+            </Header>
+        </>
     );
 }
 
